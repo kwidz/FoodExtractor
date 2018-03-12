1d59ef9b7b645b3a77aab2027f988626620d5dc5
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2018-03-12 11:43:39 -0400

    Add all items in database

--- src/ExtracteurIngredients/IGACrawler.java
+++ src/ExtracteurIngredients/IGACrawler.java
@@ -6,18 +6,17 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class IGACrawler extends Crawler {
 
-
-    //Constructor of IGA crawler
-    /*TODO
-    make a crawler function and crawl all pages
-
-     */
+    
     public IGACrawler(Trie trie){
 
         //Get the html page
+        createDB();
         Document doc = null;
         try {
             //doc = Jsoup.connect("https://www.iga.net/fr/epicerie_en_ligne/viande").get();
@@ -40,7 +39,12 @@ public class IGACrawler extends Crawler {
 
         //Get all products in the page
         Elements divs = content.getElementsByClass("item-product");
+
         for(Element product : divs){
+            String brand="";
+            String type="";
+            String name="";
+            String price="";
 
                 Elements productDetails = product.getAllElements();
 
@@ -49,11 +53,13 @@ public class IGACrawler extends Crawler {
                     if(e.hasClass("item-product__brand")){
                         System.out.println("##############################");
                         System.out.println("Brand :" +  e.text());
+                        brand=e.text();
                     }
                     if(e.is("H3")){
                         System.out.println("name : "+e.text());
+                        name=e.text();
                         String[] result = e.text().split("\\s");
-                        String type="";//=trie.bestMatch(e.text(),100);
+                        type="";//=trie.bestMatch(e.text(),100);
                         for (String word:result) {
                             if(word.length()>2) {
                                 if (trie.contains(word)) {
@@ -80,17 +86,74 @@ public class IGACrawler extends Crawler {
                     if(e.hasClass("item-product__price")){
                         System.out.println("Price :" +  e.text());
                         System.out.println("##############################");
+                        price=e.text();
                     }
                     if(e.hasClass("item-product__price--sale")){
                         System.out.println("Price (sale) :" +  e.text());
+                        price=e.text();
 
                     }
 
                 }
+                insertDB(name,brand,type,price);
 
         }
         System.out.println(divs.size());
 
 
+    }
+    public void insertDB(String name, String brand, String type, String price){
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String pattern = "\'";
+            name = name.replaceAll(pattern," ");
+            brand = brand.replaceAll(pattern," ");
+            String sql = "INSERT INTO FOOD (BRAND,NAME,TYPE,PRICE) " +
+                    "VALUES ('"+brand+"','"+name+"', '"+type+"', '"+price+"');";
+            System.out.println(sql);
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+    }
+
+    public void createDB(){
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE FOOD " +
+                    "(ID INT PRIMARY KEY," +
+                    " BRAND           TEXT    NOT NULL, " +
+                    " NAME            TEXT     NOT NULL, " +
+                    " TYPE        CHAR(50), " +
+                    " PRICE         REAL)";
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
     }
 }
