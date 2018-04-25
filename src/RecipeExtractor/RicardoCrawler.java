143486f994bb5bda5fa91dbabf7f3602ccd927b1
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2018-04-25 12:53:00 -0400

    Add machine learning module for components and recipe classification

--- src/RecipeExtractor/RicardoCrawler.java
+++ src/RecipeExtractor/RicardoCrawler.java
@@ -6,29 +6,64 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.SQLOutput;
 
 public class RicardoCrawler {
 
 
-    public RicardoCrawler(){
+    public RicardoCrawler() {
+        int i = 0;
+        while(true) {
+            i++;
+            String url = "https://www.ricardocuisine.com/themes/30-minutes/page/"+i;
+            Document doc = null;
 
-        Document doc = null;
-        try {
-            //doc = Jsoup.connect("https://www.iga.net/fr/epicerie_en_ligne/viande").get();
-            doc = Jsoup.connect("https://www.ricardocuisine.com/themes/30-minutes/page/1")
-                    .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0")
-                    .referrer("http://www.google.com")
-                    .maxBodySize(0)
-                    .get();
+            try {
+                //doc = Jsoup.connect("https://www.iga.net/fr/epicerie_en_ligne/viande").get();
+                doc = Jsoup.connect(url)
+                        .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0")
+                        .referrer("http://www.google.com")
+                        .maxBodySize(0)
+                        .get();
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //get all recipe of the page
+            Elements content = doc.getElementsByClass("picture");
+            if(content.size()==0)
+                break;
+            for (Element rec : content
+                    ) {
+
+
+                System.out.println("\n#################################\n");
+                System.out.println(rec.attr("title")+ "\n");
+                Element link = rec.select("a").first();
+                //Link to access one recipe of the page
+                String absHref = link.attr("abs:href"); // "http://jsoup.org/"
+
+                Document recipe = null;
+                try {
+                    //doc = Jsoup.connect("https://www.iga.net/fr/epicerie_en_ligne/viande").get();
+                    recipe = Jsoup.connect(absHref)
+                            .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0")
+                            .referrer("http://www.google.com")
+                            .maxBodySize(0)
+                            .get();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Elements ListComponents = recipe.getElementsByClass("form-ingredients").get(0).getElementsByTag("li");
+                for (Element e : ListComponents
+                        ) {
+                    System.out.println(e.text());
+
+                }
+            }
         }
-
-        //get all recipe of the page
-        Elements content = doc.getElementsByClass("picture");
-        System.out.println(content.get(0));
-
 
     }
 
