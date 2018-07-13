c5fd73cc0520faa42b89f81b753b62597229aa31
Geoffrey Glangine <geoffrey.glangine@gmail.com>
2018-07-13 16:10:09 -0400

    IGA's componnent classification + refactoring ingredient reader to use it with Ingredient class

--- src/ExtracteurIngredients/IGACrawler.java
+++ src/ExtracteurIngredients/IGACrawler.java
@@ -10,15 +10,25 @@ import java.io.IOException;
 public class IGACrawler extends IngredientCrawler {
 
 
-    public IGACrawler(Trie _vegetables){
-        vegetables=_vegetables;
+    public IGACrawler(){
 
-        //Get the html page
-        createDB();
+        String Vegetables = "https://www.iga.net/fr/epicerie_en_ligne/fruits_et_legumes?page=1&pageSize=60000";
+        String Meats = "https://www.iga.net/fr/epicerie_en_ligne/viande?page=1&pageSize=60000";
+        String Fish = "https://www.iga.net/fr/epicerie_en_ligne/fruits_de_mer?page=1&pageSize=60000";
+        String Other = "https://www.iga.net/fr/epicerie_en_ligne/parcourir/%c3%89picerie/N%c3%a9cessaire%20%c3%a0%20p%c3%a2tisserie?pageSize=2000";
+        crawl(Vegetables );
+        crawl(Meats );
+        crawl(Other );
+        crawl(Fish);
+
+
+    }
+    private void crawl(String toCrawl){
+        //createDB();
         Document doc = null;
         try {
             //doc = Jsoup.connect("https://www.iga.net/fr/epicerie_en_ligne/viande").get();
-            doc = Jsoup.connect("https://www.iga.net/fr/epicerie_en_ligne/fruits_et_legumes?page=1&pageSize=60000")
+            doc = Jsoup.connect(toCrawl)
                     .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0")
                     .referrer("http://www.google.com")
                     .maxBodySize(0)
@@ -44,43 +54,42 @@ public class IGACrawler extends IngredientCrawler {
             String name="";
             String price="";
 
-                Elements productDetails = product.getAllElements();
+            Elements productDetails = product.getAllElements();
 
-                //displays product name, brand, price...
-                for (Element e : productDetails){
-                    if(e.hasClass("item-product__brand")){
-                        System.out.println("##############################");
-                        System.out.println("Brand :" +  e.text());
-                        brand=e.text();
-                    }
-                    if(e.is("H3")){
-                        System.out.println("name : "+e.text());
-                        name=e.text();
-
-                        type=findVegetableType(name);//=_vegetables.bestMatch(e.text(),100);
-
-                        System.out.println("Type : "+type);
-
-                    }
-                    if(e.hasClass("item-product__price")){
-                        System.out.println("Price :" +  e.text());
-                        System.out.println("##############################");
-                        price=e.text();
-                    }
-                    if(e.hasClass("item-product__price--sale")){
-                        System.out.println("Price (sale) :" +  e.text());
-                        price=e.text();
-
-                    }
+            //displays product name, brand, price...
+            for (Element e : productDetails){
+                if(e.hasClass("item-product__brand")){
+                   // System.out.println("##############################");
+                    //System.out.println("Brand :" +  e.text());
+                    brand=e.text();
+                }
+                if(e.is("H3")){
+                    //System.out.println("name : "+e.text());
+                    name=e.text();
 
                 }
-                insertDB(name,brand,type,price);
+                if(e.hasClass("item-product__price")){
+                    //System.out.println("Price :" +  e.text());
+                   // System.out.println("##############################");
+                    price=e.text();
+                }
+                if(e.hasClass("item-product__price--sale")){
+                    //System.out.println("Price (sale) :" +  e.text());
+                    price=e.text();
+
+                }
+
+            }
+            Ingredient i = new Ingredient(name,new String( "0"),brand,price);
+            i.setType(classificator.classify(i));
+            allGroceryComponents.add(i);
+            System.out.println(i);
 
         }
         System.out.println(divs.size());
 
-
     }
+
 
 
 
